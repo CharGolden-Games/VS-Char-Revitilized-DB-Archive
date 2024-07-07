02af3a228c7bbbdf1992ba9ad40f8256c1dc2947
MC077 <cwmeinzer@icloud.com>
2024-07-06 20:23:43 -0700

    hehehehehhehehehehehehehehehheheeheheheheheheheeheheheehehehhehehehehehehhehehehheheheheheheehehehehhehee

--- source/states/stages/White.hx
+++ source/states/stages/White.hx
@@ -7,7 +7,7 @@ class White extends BaseStage
         //lmao the easiest to make stage, just 2 lines of code as of 5/28/24 but now its more complicated lmao. as of 6/17/24 its simpler
 		var whiteLmao:FlxSprite = new FlxSprite(-2500, -1000).makeGraphic(6000, 4000, FlxColor.WHITE);
 		addBehindDad(whiteLmao);
-		fallenChair = new BGSprite('fallenCharLmao');
+		fallenChair = new BGSprite('Fuck You');
 		fallenChair.x = 0 - 10;
 		fallenChair.y = dad.y;
 		fallenChair.visible = false;
