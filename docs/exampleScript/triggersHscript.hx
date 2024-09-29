c3b8d8269ac04f1fc14ddfce4e8277f6e9409b58
Anny (Char) <73309364+CharGoldenYT@users.noreply.github.com>
2024-09-29 14:45:30 -0400

    add comments to example scripts

--- docs/exampleScript/triggersHscript.hx
+++ docs/exampleScript/triggersHscript.hx
@@ -1,11 +1,11 @@
-function songNameToTrigger(songName:String):String 
+function songNameToTrigger(songName:String):String //  This converts a song name to 'Triggers <Song Name>' to match the original Marios Madness trigger system
 {
     if (songName == 'dad-battle') {
         return 'Triggers Dad Battle';
     }
 }
 
-function doTrigger(songTrigger:String, strumTime:Float, value1:Float, value2:Dynamic):Bool
+function doTrigger(songTrigger:String, strumTime:Float, value1:Float, value2:Dynamic):Bool // Actually triggers the.. trigger.
 {
     switch (songTrigger) {
         case 'Triggers Dad Battle':
